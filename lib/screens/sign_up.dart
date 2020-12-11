0e0286e2c80ee3e4e1e01186f19f68c97d470df3
amirhosein <amirhoseinheshmati@gmail.com>
2020-12-11 19:48:43 +0330

    bug fix

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,6 +1,7 @@
+import 'package:dreamer/common/dream_consumer.dart';
 import 'package:dreamer/common/widgets/dreamer_scaffold.dart';
 import 'package:dreamer/common/widgets/dreamer_text_field.dart';
-import 'package:dreamer/common/dream_consumer.dart';
+import 'package:dreamer/common/widgets/let_scroll.dart';
 import 'package:dreamer/view_models/auth_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -47,84 +48,86 @@ class _SignUpState extends State<SignUp> {
       }
     });
     return DreamerScaffold(
-      body: Container(
-        padding: EdgeInsets.all(48.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Sign up',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  DreamTextField(
-                    label: "Username",
-                    controller: usernameController,
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  DreamTextField(
-                    label: "Password",
-                    controller: passwordController,
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  DreamTextField(
-                    label: "Retype Password",
-                    controller: passwordReController,
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    child: DreamConsumer<AuthViewModel>(
-                      builder: (context, auth, child) => RaisedButton(
-                        child: Text('Sign up'),
-                        onPressed: auth.isLoading
-                            ? null
-                            : () {
-                                if (_formKey.currentState.validate()) {
-                                  auth.signUpWithPassword(
-                                    usernameController.text,
-                                    passwordController.text,
-                                    passwordReController.text,
-                                  );
-                                }
-                              },
+      body: LetScroll(
+        child: Container(
+          padding: EdgeInsets.all(48.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Sign up',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    DreamTextField(
+                      label: "Username",
+                      controller: usernameController,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    DreamTextField(
+                      label: "Password",
+                      controller: passwordController,
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    DreamTextField(
+                      label: "Retype Password",
+                      controller: passwordReController,
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: DreamConsumer<AuthViewModel>(
+                        builder: (context, auth, child) => RaisedButton(
+                          child: Text('Sign up'),
+                          onPressed: auth.isLoading
+                              ? null
+                              : () {
+                                  if (_formKey.currentState.validate()) {
+                                    auth.signUpWithPassword(
+                                      usernameController.text,
+                                      passwordController.text,
+                                      passwordReController.text,
+                                    );
+                                  }
+                                },
+                        ),
                       ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Login instead?"),
+                  MaterialButton(
+                    minWidth: 0,
+                    padding: EdgeInsets.only(left: 8, right: 8),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/login');
+                    },
+                    child: Text(
+                      "Login",
                     ),
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Login instead?"),
-                MaterialButton(
-                  minWidth: 0,
-                  padding: EdgeInsets.only(left: 8, right: 8),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/login');
-                  },
-                  child: Text(
-                    "Login",
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
